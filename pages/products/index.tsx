51c80e7685c6cada85f259e8065c1b74b22aa681
thao02122002 <vant16039@gmail.com>
2022-06-29 22:16:19 +0700

    call api list and detail

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,11 +1,41 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
+import Link from 'next/link';
 import React from 'react'
 
-type Props = {}
 
-const ProductPage = (props: Props) => {
+type ProductsProps = {
+  products: any[];
+}
+// chạy client
+const ProductPage = ({products}: ProductsProps) => {
+  console.log('Product page client');
+  console.log('products', products);
+  if(!products) return null
+  
+  
   return (
-    <div>ProductPage</div>
+    <div>{products.map((item) => (
+      <div key={item._id}><Link href={`/products/${item._id}`}>{item.name}</Link> </div>
+    ))}</div>
   )
 }
 
+
+//chạy server
+export const getStaticProps: GetStaticProps<ProductsProps> = async (
+  context: GetStaticPropsContext
+) => {
+  console.log('Get static props - server');
+  const data = await (await fetch(`http://localhost:8008/api/products`)).json()
+  console.log('data', data);
+  if(!data) return {
+    notFound: true
+  }
+  return {
+    props: {
+      products: data
+    }
+  }
+  
+}
 export default ProductPage
